2cc741d52745057482af8628f0c63ad2b5ed8d22
Danny <danny.dc.cao@gmail.com>
2021-09-04 12:26:42 +0200

    dom foutje, bug gefixt

--- APICase/Models/AdresAfstandDTO.cs
+++ APICase/Models/AdresAfstandDTO.cs
@@ -1,9 +1,12 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace APICase.Models
 {
     public class AdresAfstandDTO
     {
+        [Required]
         public AdresDTO adres1 { get; set; }
+        [Required]
         public AdresDTO adres2 { get; set; }
     }
 }
